65c25c65334d112678081ff524baa5a9c19df27f
Takewi <gawi2k21@gmail.com>
2021-12-18 22:15:21 -0300

    🔥 Remove react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import React from 'react';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { About } from './components/About';
-import { Home } from './components/Home';
 import { Layout } from './components/layout/Layout';
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route path='home' element={<Home />}/>
-          <Route path='about' element={<About />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div className="d-flex justify-content-center">
+      <Layout />
+    </div>
   )
 }
